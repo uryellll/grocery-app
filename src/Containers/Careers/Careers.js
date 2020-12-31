970fcea39863dbe4dfed4d6a6864e523fe0fbc92
uryellll <ffurio.uriel@gmail.com>
2021-01-01 07:46:54 +0800

    modified careers page

--- src/Containers/Careers/Careers.js
+++ src/Containers/Careers/Careers.js
@@ -7,37 +7,77 @@ function Careers() {
   return (
     <>
       <NavigationBar />
-      <div className={classes.container}>
-        <h1>Careers</h1>
-        <div className={classes.jobContainer}>
-          <h2>Job Title</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-            voluptatem autem placeat, doloribus dolores deleniti sint aliquam
-            quos at neque!
-          </p>
-          <h3>Requirements:</h3>
-          <ul>
-            <li>Lorem ipsum dolor sit amet.</li>
-            <li>Lorem ipsum dolor sit amet.</li>
-            <li>Lorem ipsum dolor sit amet.</li>
-            <li>Lorem ipsum dolor sit amet.</li>
-          </ul>
+      <div className={classes.Container}>
+        <div className={classes.banner}>
+          <h1>Join Our Team</h1>
         </div>
-        <div className={classes.jobContainer}>
-          <h2>Job Title</h2>
+        <div className={classes.Contents}>
+          <h2>Why join us?</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-            voluptatem autem placeat, doloribus dolores deleniti sint aliquam
-            quos at neque!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos quam
+            enim assumenda aut, in tempore porro, aliquam ea doloribus possimus
+            sequi vero dolorum quibusdam nemo, officiis sapiente mollitia rerum
+            nobis voluptatem cupiditate repellat rem? Voluptatibus itaque soluta
+            enim possimus, eveniet magni dolor praesentium officiis et, quia
+            dolorem consectetur necessitatibus esse!
           </p>
-          <h3>Requirements:</h3>
-          <ul>
-            <li>Lorem ipsum dolor sit amet.</li>
-            <li>Lorem ipsum dolor sit amet.</li>
-            <li>Lorem ipsum dolor sit amet.</li>
-            <li>Lorem ipsum dolor sit amet.</li>
-          </ul>
+        </div>
+        <h2>Available Positions</h2>
+        <div className={classes.CareersContainer}>
+          <div className={classes.Careers}>
+            <h4>Job Title</h4>
+            <h5>Job Description:</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              rerum dolor qui velit vero quam explicabo. Corrupti atque illum
+              tempora!
+            </p>
+          </div>
+          <div className={classes.Careers}>
+            <h4>Job Title</h4>
+            <h5>Job Description:</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              rerum dolor qui velit vero quam explicabo. Corrupti atque illum
+              tempora!
+            </p>
+          </div>
+          <div className={classes.Careers}>
+            <h4>Job Title</h4>
+            <h5>Job Description:</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              rerum dolor qui velit vero quam explicabo. Corrupti atque illum
+              tempora!
+            </p>
+          </div>
+          <div className={classes.Careers}>
+            <h4>Job Title</h4>
+            <h5>Job Description:</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              rerum dolor qui velit vero quam explicabo. Corrupti atque illum
+              tempora!
+            </p>
+          </div>
+          <div className={classes.Careers}>
+            <h4>Job Title</h4>
+            <h5>Job Description:</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              rerum dolor qui velit vero quam explicabo. Corrupti atque illum
+              tempora!
+            </p>
+          </div>
+          <div className={classes.Careers}>
+            <h4>Job Title</h4>
+            <h5>Job Description:</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              rerum dolor qui velit vero quam explicabo. Corrupti atque illum
+              tempora!
+            </p>
+          </div>
         </div>
       </div>
       <Footer />
