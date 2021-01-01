2840b85b2a85aa727075d885f47ecc607a2c2f2d
uryellll <ffurio.uriel@gmail.com>
2021-01-01 22:39:05 +0800

    modified FAQs page

--- src/Containers/FAQs/FAQs.js
+++ src/Containers/FAQs/FAQs.js
@@ -8,18 +8,23 @@ function FAQs() {
     <>
       <NavigationBar />
       <div className={classes.container}>
+        <h1>Frequently Asked Questions</h1>
         <details>
           <summary>Question 1</summary>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
-            quis!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil
+            dolorum eius ducimus quisquam corporis qui quae velit voluptatibus
+            provident veritatis.
           </p>
         </details>
         <details>
           <summary>Question 2</summary>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
-            quis!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+            officia impedit ipsum, nam iusto ex consectetur, minima magnam ea
+            saepe modi fuga autem. Doloribus quaerat totam rerum voluptas
+            ducimus at blanditiis culpa molestiae ratione iste. Molestias ad
+            aperiam temporibus ex.
           </p>
         </details>
         <details>
