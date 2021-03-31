d0ff40a4ce28c824e421869a7b8d7bb984d54c1e
uryellll <ffurio.uriel@gmail.com>
2021-03-31 17:58:24 +0800

    edit error page

--- src/Components/Error/Error404.js
+++ src/Components/Error/Error404.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Error.css'
 
 const Error404 = () => {
   return (
-    <div>
+    <div className="error-container">
       <h1>ERROR 404</h1>
       <Link to="/" exact>
-        Go back
+        Go back to home
       </Link>
     </div>
   )
