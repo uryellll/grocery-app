da7432aedcefdd94295584f2ef3ddc3873208beb
uryellll <ffurio.uriel@gmail.com>
2021-06-03 17:04:00 +0800

    added home button on confirmation page

--- src/Containers/Checkout Form/Confirmation/ComfirmationPage.js
+++ src/Containers/Checkout Form/Confirmation/ComfirmationPage.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { AiOutlineCheckCircle } from 'react-icons/ai'
 import './ConfirmationPage.css'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 function ComfirmationPage() {
   const checkOutInfo = useSelector((state) => state.checkOut)
@@ -11,13 +13,19 @@ function ComfirmationPage() {
 
   return (
     <div className="confirmation-container">
-      <h1>Order Confirmed</h1>
+      <div className="confirmation-logo">
+        <AiOutlineCheckCircle />
+      </div>
+      <h1>Thank you for your purchase</h1>
       <div className="customer-info">
         <h2>Customer Name:</h2>
         <p>{`${firstname} ${lastname}`}</p>
         <h2>Shipping Address:</h2>
         <p>{`${street}, ${town_city}`}</p>
       </div>
+      <Link to="/">
+        <button type="button">Back to home</button>
+      </Link>
     </div>
   )
 }
